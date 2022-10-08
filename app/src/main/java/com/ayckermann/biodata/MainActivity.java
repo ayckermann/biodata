9c3be0d1496697b2ade583350d403dc978d6e9b2
ayckermann <aya.mf12@gmail.com>
2022-10-08 22:02:46 +0700

    2nd

--- app/src/main/java/com/ayckermann/biodata/MainActivity.java
+++ app/src/main/java/com/ayckermann/biodata/MainActivity.java
@@ -2,7 +2,10 @@ package com.ayckermann.biodata;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void alamat(View view){
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("google.streetview:cbll=46.414382,10.013988"));
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
+    }
+    public void telepon(View view){
+        Intent telIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:085158305118"));
+        telIntent.setPackage("com.google.android.apps.maps");
+        startActivity(telIntent);
+    }
+    public void email(View view){
+        Intent mailIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("google.streetview:cbll=46.414382,10.013988"));
+        mailIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mailIntent);
     }
 }
